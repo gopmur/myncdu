050109d11e738d66b00272da023ca4da0e8c4ca5
Mohadese Amari <mhdseamari@gmail.com>
2023-12-31 16:20:44 +0330

    Added working threads

--- myncdu.c
+++ myncdu.c
@@ -25,7 +25,15 @@ long fsize(char *filePath) {
         printf("Error\n");
         return -1;
     }
-    
+
+}
+
+char *dircat(char *currentDirectory, char *fileName) {
+    char *filePath = (char *) malloc(strlen(currentDirectory) + 2 + strlen(fileName));
+    strcpy(filePath, currentDirectory);
+    strcat(filePath, "/");
+    strcat(filePath, fileName);
+    return filePath;
 }
 
 int main(int argc, char **argv) {
@@ -39,36 +47,45 @@ int main(int argc, char **argv) {
     DIR *dir;
     struct dirent *item;
     dir = opendir(argv[1]);
+
     if (dir) {
         while ((item = readdir(dir)) != NULL) {
-            if (item->d_type == DT_DIR && strcmp(item->d_name, ".") && strcmp(item->d_name, "..")) {     
+            if (item->d_type == DT_DIR && strcmp(item->d_name, ".") && strcmp(item->d_name, "..")) {
                 pid = fork();
                 if (pid == 0) break;
+            }
+            else if (item->d_type == DT_REG) {
+                char *filePath = getFilePath(argv[1], item->d_name);
+                long size = fsize(filePath);
+
+                printf("%s: %d\n", filePath, size);
+
+                free(filePath);
             }
         }
     }
 
     // child
     if (pid == 0) {
-        
+
         // create new dir addr in path var
-        char *currentFolder = item->d_name; 
+        char *currentFolder = item->d_name;
         int currentPathLen = strlen(argv[1]) + strlen(item->d_name) + 1;
         char *currentPath = (char *)malloc(currentPathLen + 1);
         strcpy(currentPath, argv[1]);
         strcat(currentPath, "/");
         strcat(currentPath, currentFolder);
-        
+
         // open new dir
         closedir(dir);
         dir = opendir(currentPath);
-        
-        
+
+
         while ((item = readdir(dir)) != NULL) {
             if (item->d_type == DT_REG) {
-                
-                int currentPathLen = strlen(currentPath) + 1 + strlen(item->d_name);
-                char *filePath = (char *) malloc(currentPathLen + 1);
+
+                int filePathLen = strlen(currentPath) + 1 + strlen(item->d_name);
+                char *filePath = (char *)malloc(filePathLen + 1);
 
                 strcpy(filePath, currentPath);
                 strcat(filePath, "/");
@@ -87,10 +104,19 @@ int main(int argc, char **argv) {
 
     // parent
     else {
+
         // wait for all children to finish
+
+
+        while ((item = readdir(dir) != NULL)) {
+
+
+
+        }
+
         closedir(dir);
         while (wait(NULL) > 0);
-        
+
     }
 
     return 0;
